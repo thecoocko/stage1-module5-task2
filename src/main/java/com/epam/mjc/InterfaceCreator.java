6468dd78055a07e7fd2a99a851a1165d49df768b
thecoocko <butenkonaddezhda@gmail.com>
2023-08-04 13:12:53 +0200

    done

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,27 @@
 package com.epam.mjc;
 
-public class InterfaceCreator {
+import java.util.ArrayList;
+import java.util.List;
 
+public class InterfaceCreator {
+    /**
+     * Implement a program which have your own functional interface according code below
+     *
+     * @FunctionalInterface
+     * public interface Operation<T> {
+     *     List<T> apply(List<T> arg);
+     * }
+     * Implement a class InterfaceCreator with one method Operation<Integer>
+     *     divideBy(Integer divider).
+     *     This method return your functional interface with
+     *     logic where creates new list with values from the first list divided on divider of method.*/
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (List<Integer> args)->{
+            List<Integer> result = new ArrayList<>();
+            for(int i: args){
+                result.add(i/divider);
+            }
+            return result;
+        };
     }
 }
